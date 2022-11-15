a45d2116ed4180018b8d3e56be5805b7c12f2968
Rudyprawiran <rudypn20@gmail.com>
2022-11-15 19:21:04 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Detail from "./components/Detail";
-
+      
 function App() {
   return (
     <div className="App">
