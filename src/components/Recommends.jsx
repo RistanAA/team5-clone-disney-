cc5075844b2cc06790f824bcc2406cf7d118bfbb
Ibnu Abdillah <abdibnu@gmail.com>
2022-11-15 17:33:37 +0700

    slick_home

--- src/components/Recommends.jsx
+++ src/components/Recommends.jsx
@@ -1,80 +1,168 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+
 // import { useSelector } from "react-redux";
 // import { selectRecommend } from "../features/movie/movieSlice";
 
 const Recommends = (props) => {
-//   const movies = useSelector(selectRecommend);
-//   console.log(movies, ":🛢️");
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 8
+  }
+  //   const movies = useSelector(selectRecommend);
+  //   console.log(movies, ":🛢️");
 
   return (
     <Container>
-      <h4>Recommended for You</h4>
-      <Content>
-        {/* {movies &&
+      <h3>Pilihan Top Untukmu</h3>
+      {/* <Content> */}
+        <Carousel {...settings}>
+          {/* {movies &&
           movies.map((movie, key) => ( */}
-            {/* <Wrap key={key}> */}
-            <Wrap>
-              {/* {movie.id} */}
-              {/* <Link to={`/detail/` + movie.id}> */}
-              <Link to = "/">
-                {/* <img src={movie.cardImg} alt={movie.title} /> */}
-                <img src = "https://img1.hotstarext.com/image/upload/f_auto,t_hcdl/sources/r1/cms/prod/5040/1405040-h-745f9aa52bbc" alt = "" />
-              </Link>
-            </Wrap>
+          {/* <Wrap key={key}> */}
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/6625/746625-v" alt="" />
+            </Link>
+          </Wrap>
           {/* ))} */}
 
           <Wrap>
-              {/* {movie.id} */}
-              {/* <Link to={`/detail/` + movie.id}> */}
-              <Link to = "/">
-                {/* <img src={movie.cardImg} alt={movie.title} /> */}
-                <img src = "https://images.freekaamaal.com/post_images/1598267970.jpg" alt = "" />
-              </Link>
-            </Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/8258/1328258-v-d286a921a890" alt="" />
+            </Link>
+          </Wrap>
 
-            <Wrap>
-              {/* {movie.id} */}
-              {/* <Link to={`/detail/` + movie.id}> */}
-              <Link to = "/">
-                {/* <img src={movie.cardImg} alt={movie.title} /> */}
-                <img src = "https://starsubscription.com/wp-content/uploads/2021/03/Screenshot-2021-03-28-at-10.24.22-AM.png" alt = "" />
-              </Link>
-            </Wrap>
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7610/1387610-v-eeeb3062aa0b" alt="" />
+            </Link>
+          </Wrap>
 
-            <Wrap>
-              {/* {movie.id} */}
-              {/* <Link to={`/detail/` + movie.id}> */}
-              <Link to = "/">
-                {/* <img src={movie.cardImg} alt={movie.title} /> */}
-                <img src = "https://images.freekaamaal.com/post_images/1583481356.png" alt = "" />
-              </Link>
-            </Wrap>
-      </Content>
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7288/817288-v" alt="" />
+            </Link>
+          </Wrap>
+
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7288/817288-v" alt="" />
+            </Link>
+          </Wrap>
+
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7288/817288-v" alt="" />
+            </Link>
+          </Wrap>
+
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7288/817288-v" alt="" />
+            </Link>
+          </Wrap>
+
+          <Wrap>
+            {/* {movie.id} */}
+            {/* <Link to={`/detail/` + movie.id}> */}
+            <Link to="/">
+              {/* <img src={movie.cardImg} alt={movie.title} /> */}
+              <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7288/817288-v" alt="" />
+            </Link>
+          </Wrap>
+        </Carousel>
+      {/* </Content> */}
     </Container>
+
   );
 };
 
-const Container = styled.div`
-  padding: 0 0 26px;
-`;
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+  padding left: 200px;
+  height: 200px;
+  z-index: 3;
 
-const Content = styled.div`
-  display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
 
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
   }
 `;
 
+const Container = styled.div`
+  padding: 0 0 5px;
+  height: 300px;
+`;
+
+// const Content = styled.div`
+//   display: flex;
+//   gap : 7px;
+//   width: 100%;
+//   height: 100%;
+// `;
+
 const Wrap = styled.div`
-  padding-top: 56.25%;
-  border-radius: 10px;
+  padding-top: 30px;
+  width: 150px;
+  height: 200px;
+  border-radius: 5px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
@@ -97,8 +185,10 @@ const Wrap = styled.div`
   &:hover {
     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-    transform: scale(1.05);
-    border-color: rgba(249, 249, 249, 0.8);
+    transform: scale(1.2);
+    z-index: 2;
+    border:2px;
+    opacity: 0.9;
   }
 `;
 
